c3a308da2da519712ff60436066719596812b4fc
albert <wlj_3322@163.com>
2017-01-09 19:04:03 +0800

    添加二维码地址解码

--- src/views/qrcode/decode.vue
+++ src/views/qrcode/decode.vue
@@ -18,6 +18,9 @@
                             <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
                         </el-upload>
                     </el-form-item>
+                    <el-form-item label="链接">
+                        <el-input v-model="imgurl" @blur="handleImgBlur"></el-input>
+                    </el-form-item>
                     <el-form-item label="解码">
                         <el-input type="textarea" v-model="form.decodetext" :rows="4" :spellcheck="false"></el-input>
                     </el-form-item>
@@ -39,6 +42,7 @@
                 form: {
                     decodetext: ""
                 },
+                imgurl: "",
                 fileList: [],
                 imgdata: "",
                 dialogVisible: false
@@ -46,11 +50,49 @@
         },
         mounted() {
             qrcode.callback = (result) => {
-                console.log(result);
                 this.form.decodetext = result;
             }
         },
         methods: {
+            handleImgBlur(e) {
+                var reg = /^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$/;
+                if (reg.test(this.imgurl)) {
+                     //qrcode自带
+                    var domain = this.imgurl.match(/(http|https|ftp):\/\/([^\/]+)\//i);
+                    domain = domain ? domain[2] : "未知";
+                    this.decodeImg(`来自 ${domain} 的图片`, this.imgurl);
+
+                    /* 设置了跨域的图片不能生成base64
+                    var img = document.createElement("img");
+                    img.crossOrigin = "Anonymous";
+                    var _ = this;
+                    console.log(1);
+                    try {
+                        img.onload = function() {
+                            console.log(4);
+
+                            var canvas = document.createElement("canvas");
+                            canvas.width = this.width;
+                            canvas.height = this.height;
+
+                            var ctx = canvas.getContext("2d");
+                            ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+                            //document.body.appendChild(canvas);
+
+                            console.log(5);
+                            var dataURL = canvas.toDataURL("image/png");
+                            _.decodeImg(_.imgurl, dataURL);
+
+                        }
+                        console.log(2);
+                        img.src = this.imgurl;
+                        console.log(3);
+                    } catch (err) {
+                        console.log(123);
+                    }*/
+                }
+                console.log(this.imgurl);
+            },
             handleRemove(file, fileList) {
                 //console.log(file, fileList);
                 this.imgdata = "";
@@ -59,16 +101,19 @@
             handlePreview(file) {
                 this.dialogVisible = true;
             },
+            decodeImg(filename, result) {
+                this.imgdata = result;
+                this.fileList = [{
+                    name: filename,
+                    url: result
+                }];
+                qrcode.decode(result);
+            },
             handleUpload(file) {
                 var _ = this;
                 var reader = new FileReader();
                 reader.onload = function(f) {
-                    _.imgdata = f.target.result;
-                    _.fileList = [{
-                        name: file.name,
-                        url: f.target.result
-                    }];
-                    qrcode.decode(_.imgdata);
+                    _.decodeImg(file.name, f.target.result);
                 }
                 reader.readAsDataURL(file);
                 //this.fileList=[file];
